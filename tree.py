454885b217efaca1b0491a0f549af5fd74e53632
ranmx <ran.gabriele@live.com>
2016-02-24 16:37:57 +0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,541 @@
+class TreeBase(object):
+    def __init__(self):
+        self._size = 0
+        self._root = None
+
+    class Position(object):
+
+        def element(self):
+            raise NotImplementedError
+
+        def __eq__(self, other):
+            raise NotImplementedError
+
+        def __ne__(self, other):
+            return not (self == other)
+
+    def root(self):
+        raise NotImplementedError
+
+    def is_root(self, p):
+        return p == self.root()
+
+    def parent(self, p):
+        raise NotImplementedError
+
+    def num_children(self, p):
+        raise NotImplementedError
+
+    def children(self, p):
+        raise NotImplementedError
+
+    def is_leaf(self, p):
+        return 0 == self.num_children(p)
+
+    def is_empty(self):
+        return self._size == 0
+
+    def positions(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        return self._size
+
+    def depth(self, p):
+        if self.is_root(p):
+            return 0
+        else:
+            parent = self.parent(p)
+            depth = self.depth(parent) + 1
+            return depth
+
+    def _height(self, p):
+        if self.is_leaf(p):
+            height = 0
+        else:
+            height = 1 + max(self._height(c) for c in self.children(p))
+        return height
+
+    def height(self, p=None):
+        if p is None:
+            return self._height(self.root())
+        else:
+            return self._height(p)
+
+
+class LinkTree(TreeBase):
+    def __init__(self):
+        super(LinkTree, self).__init__()
+
+    class Position(TreeBase.Position):
+        __slots__ = 'container', 'node'
+
+        def __init__(self, container, node):
+            self.container = container
+            self.node = node
+
+        def element(self):
+            return self.node._element
+
+        def __eq__(self, other):
+            return (type(self) == type(other)) and (self.element() == other.element())
+
+    def _make_position(self, node):
+        if not isinstance(node, self.Node):
+            raise TypeError('%s is not a node!' % node)
+        else:
+            return self.Position(self, node)
+
+    def _is_valid(self, p):
+        if not isinstance(p, self.Position):
+            raise TypeError(p, 'is not a Position!')
+        elif p.container != self:
+            raise ValueError(p, 'is not contained in', self)
+        elif p.element() is None:
+            raise ValueError(p, 'is not valid!')
+        return p.node
+
+    def add_root(self, e):
+        if self.root() is not None:
+            raise ValueError("There is already a root!")
+        else:
+            self._root = self.Node(e)
+            self._size = 1
+            return self._make_position(self._root)
+
+    def root(self):
+        if self.is_empty():
+            return None
+        else:
+            return self._make_position(self._root)
+
+    def parent(self, p):
+        node = self._is_valid(p)
+        if node._parent is not None:
+            return self._make_position(node._parent)
+        return None
+
+    def replace(self, p, element):
+        node = self._is_valid(p)
+        node._element = element
+        return p
+
+    def is_leaf(self, p):
+        return 0 == self.num_children(p)
+
+    def is_empty(self):
+        return self._size == 0
+
+    def __len__(self):
+        return self._size
+
+    def depth(self, p):
+        if self.is_root(p):
+            return 0
+        else:
+            parent = self.parent(p)
+            depth = self.depth(parent) + 1
+            return depth
+
+    def _height(self, p):
+        if self.is_leaf(p):
+            height = 0
+        else:
+            height = 1 + max(self._height(c) for c in self.children(p))
+        return height
+
+    def height(self, p=None):
+        if p is None:
+            return self._height(self.root())
+        else:
+            return self._height(p)
+
+    def _deepfirst(self, p, d, path):
+        self._is_valid(p)
+        self._dprehandle(p, d, path)
+        path.append(0)
+        for child in self.children(p):
+            path[-1] += 1
+            self._deepfirst(child, d+1, path)
+        path.pop()
+        result = self._dposthandle(p, d, path)
+        return result
+
+    def _broadfirst(self):
+        traversal_list = []
+        traversal_list.append(self.root())
+        while len(traversal_list) != 0:
+            p = traversal_list.pop()
+            self._bprehandle(p)
+            for child in self.children(p):
+                traversal_list.append(child)
+            self._bposthandle(p)
+
+    def _dprehandle(self, p, d, path):
+        pass
+
+    def _dposthandle(self, p, d, path):
+        return None
+        pass
+
+    def _bprehandle(self, p):
+        pass
+
+    def _bposthandle(self, p):
+        return None
+        pass
+
+    def positions(self, broad_first=False):
+        if not broad_first:
+            self._deepfirst(self.root(), 1, [])
+        else:
+            self._broadfirst()
+
+
+class BiTreeBase(LinkTree):
+    def __init__(self):
+        super(BiTreeBase, self).__init__()
+
+    class Node(object):
+        __slots__= '_element', '_parent', '_left', '_right'
+
+        def __init__(self, element, parent=None, left=None, right=None):
+            self._element = element
+            self._parent = parent
+            self._left = left
+            self._right = right
+
+    def left(self, p):
+        raise NotImplementedError
+
+    def right(self, p):
+        raise NotImplementedError
+
+    def sibling(self, p):
+        if p == self.root():
+            return None
+        parent = self.parent(p)
+        if p == self.left(parent):
+            return self.left(parent)
+        else:
+            return self.right(parent)
+
+    def children(self, p):
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
+
+    def num_children(self, p):
+        count = 0
+        for child in self.children(p):
+            count += 1
+        return count
+
+
+class BinaryTree(BiTreeBase):
+    def __init__(self):
+        super(BinaryTree, self).__init__()
+
+    def left(self, p):
+        node = self._is_valid(p)
+        if node._left is not None:
+            return self._make_position(node._left)
+        else:
+            return None
+
+    def right(self, p):
+        node = self._is_valid(p)
+        if node._right is not None:
+            return self._make_position(node._right)
+        return None
+
+    def add_left(self, p, element):
+        node = self._is_valid(p)
+        if node._left is not None:
+            raise ValueError('The node already has a left node')
+        new_node = self.Node(element, node)
+        node._left = new_node
+        self._size += 1
+        return self._make_position(new_node)
+
+    def add_right(self, p, element):
+        node = self._is_valid(p)
+        if node._right is not None:
+            raise ValueError('The node already has a right node ')
+        new_node = self.Node(element, node)
+        node._right = new_node
+        self._size += 1
+        return self._make_position(new_node)
+
+    def _delete(self, p):
+        node = self._is_valid(p)
+        if self.num_children(p) > 1:
+            raise ValueError('There are more than 2 children')
+        else:
+            abandoned = p
+            orphans = node._left if node._left else node._right
+            self._size -= 1
+            if p == self.root():
+                self._root = orphans
+            else:
+                parent = self.parent(p)
+                if self.left(parent) == p:
+                    parent.node._left = orphans
+                else:
+                    parent.node._right = orphans
+                orphans.node._parent = parent
+                node._parent = node
+                return abandoned.element()
+
+    def attach(self, p, t1, t2=None):
+        node = self._is_valid(p)
+        if not (type(self) == type(t1)):
+            raise TypeError('The input must be an tree')
+        if t2 is not None:
+            if not (type(self) == type(t2)):
+                raise TypeError('The input must be an tree')
+            if not self.is_leaf(p):
+                raise ValueError("The node doesn't have enough space")
+            if not t1.is_empty():
+                node._left = t1._root
+            if not t2.is_empty():
+                node._right = t2._root
+                t2._root._parent = node
+                self._size += len(t2)
+                t2._root = None
+                t2._size = 0
+        else:
+            num_children = self.num_children(p)
+            if num_children == 0:
+                if not t1.is_empty():
+                    node._left = t1._root
+            if num_children == 1:
+                if not t1.is_empty():
+                    if node._left is not None:
+                        node._left = t1._root
+                    else:
+                        node._right = t1._root
+            else:
+                raise ValueError('The node is full')
+        t1._root._parent = node
+        self._size += len(t1)
+        t1._root = None
+        t1._size = 0
+
+    def _inorder(self, p, d, path):
+        node = self._is_valid(p)
+        if self.left(p) is not None:
+            path.append(0)
+            self._inorder(self.left(p), d+1, path)
+            path.pop(-1)
+
+        self._inorder_func(p, d+1, path)
+
+        if self.right(p) is not None:
+            path.append(1)
+            self._inorder(self.right(p), d+1, path)
+            path.pop(-1)
+
+    def _inorder_func(self, p, d, path):
+        path = map(str, path)
+        mark = '.'.join(path)
+        print d*'  ', '(', mark, ')', p.element()
+
+    def positions(self):
+        return self._inorder(self.root(), 0, [1])
+
+
+class GeneralTree(LinkTree):
+    def __init__(self):
+        super(GeneralTree, self).__init__()
+
+    class Node(object):
+        __slots__ = '_element', '_parent', '_children'
+
+        def __init__(self, element, parent=None):
+            self._element = element
+            self._parent = parent
+            self._children = []
+
+    def children(self, p):
+        node = self._is_valid(p)
+        for child in node._children:
+            yield self._make_position(child)
+
+    def num_children(self, p):
+        node = self._is_valid(p)
+        return len(node._children)
+
+    def add_child(self, p, element):
+        node = self._is_valid(p)
+        child = self.Node(element, node)
+        node._children.append(child)
+        self._size += 1
+        return self._make_position(child)
+
+    def _delete(self, p):
+        node = self._is_valid(p)
+        abandon = node._element
+        if node == self._root:
+            self._root = None
+            self._size = 0
+        else:
+            parent = node._parent
+            parent._children.remove(node)
+            if self.num_children(p) > 0:
+                orphans = node._children
+                parent._children.extend(orphans)
+                for child in orphans:
+                    child._parent = parent
+            self._size -= 1
+        return abandon
+
+    def attach(self, p, tree):
+        node = self._is_valid(p)
+        if not (isinstance(tree, BinaryTree) or isinstance(tree, GeneralTree)):
+            raise TypeError('There must be a tree')
+        node._children.append(tree._root)
+        tree._root._parent = node
+        self._size += len(tree)
+        tree._root = None
+        tree._size = 0
+
+    def _dprehandle(self, p, d, path):
+        mark = '.'.join(map(str, path))
+        print '    '*d, "(", mark, ")", p.element()
+
+def test_binary_tree():
+    bt = BinaryTree()
+    root = bt.add_root(5)
+    bt_left = bt.add_left(root, 3)
+    bt_right = bt.add_right(root, 7)
+    bt1 = BinaryTree()
+    root1 = bt1.add_root(15)
+    bt1.add_left(root1, 13)
+    bt1.add_right(root1, 17)
+    bt2 = BinaryTree()
+    root2 = bt2.add_root(25)
+    bt2.add_left(root2, 23)
+    bt2.add_right(root2, 27)
+    print "-"*40
+    bt.positions()
+    bt.attach(bt_left, bt1, bt2)
+    print "-"*40
+    bt.positions()
+    print "-"*40
+    print 'size of bt:', len(bt)
+    print "-"*40
+    print 'root of bt:', bt.root().element()
+    print "-"*40
+    bt_root_left = bt.left(root)
+    print 'left node of root:', bt_root_left.element()
+    print "-"*40
+    bt_root_right = bt.right(root)
+    print 'right node of root:', bt_root_right.element()
+    print "-"*40
+    bt_root_parent = bt.parent(root)
+    print 'parent of root:', bt_root_parent
+    print "-"*40
+    bt_root_left_partent = bt.parent(bt_root_left)
+    print 'left node of root:', bt_root_left_partent.element()
+    print "-"*40
+    bt_root = bt.replace(bt.root(), 42)
+    print 'replaced root:', bt_root.element()
+    print "-"*40
+    bt.positions()
+    print "-"*40
+    print "depth of the root's left node", bt.depth(bt_root_left)
+    print "-"*40
+    print "height of the root's left node", bt.height(bt_root_left)
+
+
+def test_general_tree():
+    gt = GeneralTree()
+    root = gt.add_root(5)
+    jarry = gt.add_child(root, 1)
+    kate = gt.add_child(root, 3)
+    tom = gt.add_child(root, 7)
+    gt.add_child(tom, 10)
+    gt.add_child(tom, 12)
+    gt.add_child(tom, 15)
+    gt.add_child(jarry, 20)
+    gig = gt.add_child(jarry, 22)
+    gt.add_child(jarry, 25)
+    may = gt.add_child(kate, 30)
+    gt.add_child(kate, 32)
+    gt.add_child(kate, 35)
+    delete = kate
+    print "-"*40
+    for child in gt.children(root):
+        print 'chidren of root:', child.element()
+
+    print "-"*40
+    gt.positions()
+
+    gt1 = GeneralTree()
+    root = gt1.add_root(5)
+    jarry = gt1.add_child(root, 1)
+    kate = gt1.add_child(root, 3)
+    tom = gt1.add_child(root, 7)
+    gt1.add_child(tom, 10)
+    gt1.add_child(tom, 12)
+    gt1.add_child(tom, 15)
+    gt1.add_child(jarry, 20)
+    gt1.add_child(jarry, 22)
+    gt1.add_child(jarry, 25)
+    gt1.add_child(kate, 30)
+    gt1.add_child(kate, 32)
+    gt1.add_child(kate, 35)
+
+    gt.attach(may, gt1)
+    print "-"*40
+    gt.positions()
+
+    print "-"*40
+    print 'size of bt:', len(gt)
+    print "-"*40
+    print 'root of bt:', gt.root().element()
+    print "-"*40
+    print "depth of may", gt.depth(may)
+    print "-"*40
+    print "height of may", gt.height(may)
+    print "-"*40
+    print "delete gig", gt._delete(delete)
+    gt.positions()
+
+
+test_general_tree()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
