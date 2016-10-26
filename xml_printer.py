3b95a84f7602782efa437529f82e6f689b684861
ranmx <ran.gabriele@live.com>
2016-10-26 13:39:12 +0800

    Update xml_printer.py

--- xml_printer.py
+++ xml_printer.py
@@ -6,7 +6,6 @@ class XMLPaser(object):
     def __init__(self, path):
         self.path = path
         self.tree = ET.parse(self.path)
-        self.name = self.path.split('/')[-1].split('.')[0].title()
         self.root = self.tree.getroot()
         self.result_list = []
 
@@ -24,26 +23,26 @@ class XMLPaser(object):
                     result_dict[node.tag][child.tag] = \
                         self._deep_first_travel(child, result_dict[node.tag], child_limit)
 
-    def display(self, display_limit=None, search_limit=None):
+    def display(self, display_limit=None, search_limit=None, *args, **kwargs):
         for node in self.root:
             result_dict = OrderedDict()
-            self._deep_first_travel(node, result_dict, display_limit)
+            self._deep_first_travel(node, result_dict, search_limit)
             n = 1
-            if search_limit:
-                if search_limit != 'diy':
-                    self._select_display(result_dict, search_limit, n)
+            if display_limit:
+                if display_limit != 'diy':
+                    self._select_display(result_dict, display_limit, n)
                 else:
-                    self._select_handler()
+                    self._select_handler(result_dict, *args, **kwargs)
             else:
-                self.result_list.append(result_dict[self.name])
+                self.result_list.append(result_dict)
         if not self.result_list or len(self.result_list) == 0:
             print "no result"
-        if isinstance(self.result_list[0], str):
-            for element in sorted(set(self.result_list)):
-                print element
-        else:
-            for element in self.result_list:
-                print element
+
+        self._print()
+
+    def _print(self):
+        for element in self.result_list:
+            print element
 
     def _select_display(self, result_dict, child_limit, n):
         if n < len(child_limit)-1:
@@ -52,13 +51,48 @@ class XMLPaser(object):
         else:
             self.result_list.append(result_dict[child_limit[n]])
 
-    def _select_handler(self):
+    def _select_handler(self, result_dict, *args, **kwargs):
         pass
 
 
-# xmlparser = XMLPaser('/home/alfred/Downloads/451_Metadata.xml')
+
+
+class MetadataParser(XMLPaser):
+    def __init__(self, path):
+        super(MetadataParser, self).__init__(path)
+
+    def _print(self):
+        if isinstance(self.result_list[0], str):
+            for element in sorted(set(self.result_list)):
+                print element
+
+    def _select_handler(self, result_dict, *args, **kwargs):
+        pass
+
+    def _select_display(self, result_dict, child_limit, n):
+        if n < len(child_limit) - 1:
+            temp_dict = result_dict[child_limit[n]]
+            self._select_display(temp_dict, child_limit, n + 1)
+        else:
+            self.result_list.append(result_dict[child_limit[n]])
+
+# xmlparser = MetadataParser('/home/alfred/Downloads/451_Metadata.xml')
 # xmlparser.display(['root', 'Field', 'SystemName'], ['root', 'Field', 'SystemName'])
 
-# xmlparser = XMLPaser('/home/alfred/Downloads/501/njgsmls/residential.xml')
-# xmlparser.display()
+
+class MLSXMLPaser(XMLPaser):
+    def __init__(self, path):
+        super(MLSXMLPaser, self).__init__(path)
+        self.name = self.path.split('/')[-1].split('.')[0].title()
+        self.result_list = set()
+
+    def _select_handler(self, result_dict, *args, **kwargs):
+        self.result_list = self.result_list.union(set(result_dict[self.name].keys()))
+
+    def _print(self):
+        for element in sorted(set(self.result_list)):
+            print element
+
+xmlparser = MLSXMLPaser('/home/alfred/Downloads/501/njgsmls/residential.xml')
+xmlparser.display(display_limit='diy')
 
